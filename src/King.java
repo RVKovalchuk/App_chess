afa03e0718c9da0c2e3959c2f00491e1161c9ee2
RV.Kovalchuk <rv.kovalchuk@tut.by>
2021-12-28 23:33:39 +0300

    I added a Queen, King chessPiece.

---
+++ src/King.java
@@ -0,0 +1,48 @@
+public class King extends ChessPiece {
+
+    public King(String color) {
+        super(color);
+    }
+
+    @Override
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+        if (toColumn > 7 || toLine > 7 || toColumn < 0 || toLine < 0) {
+            System.out.println("You can't go to outside of chessBoard");
+            return false;
+        }
+
+        if ((toColumn - column == 0) && (toLine - line == 0)) {
+            System.out.println("You must change position of your chessPiece");
+            return false;
+        }
+
+        if ((toColumn - column == 0 || Math.abs(toColumn - column) == 1) && (toLine - line == 0 ||
+                Math.abs(toLine - line) == 1)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean isUnderAttack(ChessBoard board, int line, int column) {
+        for (int i = 0; i < board.board.length; i++){
+            for (int j = 0; j < board.board[i].length; j++) {
+                if (board.board[i][j] != null && !board.board[i][j].getColor().equals(this.color) &&
+                        board.board[i][j].canMoveToPosition(board, i, j, line, column)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String getSymbol() {
+        return "K";
+    }
+}
---
+++ src/Queen.java
@@ -0,0 +1,36 @@
+public class Queen extends ChessPiece {
+
+    public Queen(String color) {
+        super(color);
+    }
+
+    @Override
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+        if (toColumn > 7 || toLine > 7 || toColumn < 0 || toLine < 0) {
+            System.out.println("You can't go to outside of chessBoard");
+            return false;
+        }
+
+        if ((toColumn - column == 0) && (toLine - line == 0)) {
+            System.out.println("You must change position of your chessPiece");
+            return false;
+        }
+
+        if (((toColumn - column != 0) && (toLine - line == 0)) || ((toColumn - column == 0) && (toLine - line != 0)) ||
+                (Math.abs(toColumn - column) == Math.abs(toLine - line))) {
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public String getSymbol() {
+        return "Q";
+    }
+}
