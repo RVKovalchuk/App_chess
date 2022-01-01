e4bbcf2e33526de6df7dc925b0641df409f62e38
RV.Kovalchuk <rv.kovalchuk@tut.by>
2022-01-01 19:20:51 +0300

    I added the main method and updated CORRECT logic of opportunity to move to position of another chessPiece.

--- src/Bishop.java
+++ src/Bishop.java
@@ -12,8 +12,8 @@ public class Bishop extends ChessPiece {
     @Override
     public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
         if (toColumn <= 7 && toLine <= 7 && toColumn >= 0 && toLine >= 0) {
-            if (toColumn - column != 0 && toLine - line != 0) {
-                if (!chessBoard.board[toLine][toColumn].getColor().equals(this.color) ||
+            if (!(toColumn - column == 0 && toLine - line == 0)) {
+                if (!(chessBoard.board[toLine][toColumn].getColor().equals(this.color)) ||
                         chessBoard.board[toLine][toColumn] == null) {
                     return Math.abs(toColumn - column) == Math.abs(toLine - line);
                 }
--- src/Rook.java
+++ src/Rook.java
@@ -12,8 +12,8 @@ public class Rook extends ChessPiece {
     @Override
     public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
      if (toColumn <= 7 && toLine <= 7 && toColumn >= 0 && toLine >= 0) {
-          if ((toColumn - column != 0) || (toLine - line != 0)) {
-              if (!chessBoard.board[toLine][toColumn].getColor().equals(this.color) ||
+          if (!(toColumn - column == 0 && toLine - line == 0)) {
+              if (!(chessBoard.board[toLine][toColumn].getColor().equals(this.color)) ||
                       chessBoard.board[toLine][toColumn] == null) {
                   return ((toColumn - column != 0 && toLine - line == 0)) || ((toColumn - column == 0 && toLine - line != 0));
               }
