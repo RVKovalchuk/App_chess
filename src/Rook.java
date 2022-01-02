96eba832674ec75a9fdcb29668e8852adbbe92c1
RV.Kovalchuk <rv.kovalchuk@tut.by>
2022-01-02 22:24:26 +0300

    Updated logic of canMoveToPosition method of the Rook chessPieces.

--- src/Rook.java
+++ src/Rook.java
@@ -11,14 +11,39 @@ public class Rook extends ChessPiece {
 
     @Override
     public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
-     if (toColumn <= 7 && toLine <= 7 && toColumn >= 0 && toLine >= 0) {
-          if (!(toColumn - column == 0 && toLine - line == 0)) {
-              if (!(chessBoard.board[toLine][toColumn].getColor().equals(this.color)) ||
-                      chessBoard.board[toLine][toColumn] == null) {
-                  return ((toColumn - column != 0 && toLine - line == 0)) || ((toColumn - column == 0 && toLine - line != 0));
-              }
-          }
-     }
+        if (toColumn <= 7 && toLine <= 7 && toColumn >= 0 && toLine >= 0) {
+            if (!(toColumn - column == 0 && toLine - line == 0)) {
+                if (toColumn - column > 0 && toLine - line == 0 &&
+                        (chessBoard.board[toLine][toColumn] == null || !(chessBoard.board[toLine][toColumn].getColor().equals(this.color)))) {
+                    for (int i = column; i < (toColumn); i++) {
+                        if (chessBoard.board[line][i] == null) {
+                            return true;
+                        }
+                    }
+                } else if (toColumn - column == 0 && toLine - line > 0 &&
+                        (chessBoard.board[toLine][toColumn] == null || !(chessBoard.board[toLine][toColumn].getColor().equals(this.color)))) {
+                    for (int i = line; i < (toLine); i++) {
+                        if (chessBoard.board[i][column] == null) {
+                            return true;
+                        }
+                    }
+                } else if (toColumn - column < 0 && toLine - line == 0 &&
+                        (chessBoard.board[toLine][toColumn] == null || !(chessBoard.board[toLine][toColumn].getColor().equals(this.color)))) {
+                    for (int i = column; i > (toColumn); i--) {
+                        if (chessBoard.board[line][i] == null) {
+                            return true;
+                        }
+                    }
+                } else if (toColumn - column == 0 && toLine - line < 0 &&
+                        (chessBoard.board[toLine][toColumn] == null || !(chessBoard.board[toLine][toColumn].getColor().equals(this.color)))) {
+                    for (int i = line; i > (toLine); i--) {
+                        if (chessBoard.board[i][column] == null) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
         return false;
     }
 
